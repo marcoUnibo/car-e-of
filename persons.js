c53547bd53b81fa69b1dfc9cff5a30cf441bbfb5
Marco <50715236+marcofurno@users.noreply.github.com>
2022-07-03 15:34:52 +0200

    list of users

---
+++ persons.js
@@ -0,0 +1,153 @@
+const persone = [
+    {
+        id: "139284",
+        userid: "mbianchi",
+        name: "Marco",
+        surname: "Bianchi", 
+        address: {
+            street: "via dell'arcoveggio, 50",
+            town: "Gambettola",
+            province: "Forlì-Cesena",
+            CAP: "47143"
+        },
+        Tel: "3343233455",
+        role: "transpVol"
+    },
+
+    {
+        id: "136485",
+        userid: "sverdi",
+        name: "Silvia",
+        surname: "Verdi",
+        address: {
+            street: "via dell'arcoveggio, 50",
+            town: "Ravenna",
+            province: "Ravenna",
+            CAP: "48174"
+        },
+        Tel: "3334515665",
+        role: "transpVol"
+    },
+
+    {
+        id: "125457",
+        userid: "lrossi",
+        name: "Luca",
+        surname: "Rossi",
+        address: {
+            street: "via del foglio, 250",
+            town: "Forlì",
+            province: "Forlì",
+            CAP: "47122"
+        },
+        Tel: "332346236",
+        role: "transpVol"
+    },
+
+    {
+        id: "111453",
+        userid: "gneri",
+        name: "Giulia",
+        surname: "Neri",
+        address: {
+            street: "via del miglio rosso, 20",
+            town: "Montebello",
+            province: "Rimini",
+            CAP: "49144"
+        },
+        Tel: "323462574",
+        role: "patient"
+    },
+
+    {
+        id: "128453",
+        userid: "mgrigi",
+        name: "Matteo",
+        surname: "Grigi",
+        address: {
+            street: "via Saltello, 34",
+            town: "Roverello",
+            province: "Rimini",
+            CAP: "46174"
+        },
+        Tel: "342564565",
+        role: "patient"
+    },
+
+    {
+        id: "1285467",
+        userid: "lgialli",
+        name: "Livia",
+        surname: "Gialli",
+        address: {
+            street: "viale Dante, 11",
+            town: "Montaletto di Cervia",
+            province: "Ravenna",
+            CAP: "48123"
+        },
+        Tel: "345343465",
+        role: "patient"
+    },
+
+    {
+        id: "116344",
+        userid: "farancioni",
+        name: "Felice",
+        surname: "Arancioni",
+        address: {
+            street: "viale Dante, 65",
+            town: "Forlì",
+            province: "Forlì-Cesena",
+            CAP: "47123"
+        },
+        Tel: "34525465",
+        role: "operator"
+    },
+
+    {
+        id: "93262",
+        userid: "lblu",
+        name: "Lara",
+        surname: "Blu",
+        address: {
+            street: "viale Montale, 25",
+            town: "Forlì",
+            province: "Ravenna",
+            CAP: "47121"
+        },
+        Tel: "345222465",
+        role: "operator"
+    },
+
+    {
+        id: "92535",
+        userid: "svioletti",
+        name: "Sara",
+        surname: "Violetti",
+        address: {
+            street: "viale Picasso, 23",
+            town: "Cesena",
+            province: "Forlì-Cesena",
+            CAP: "47129"
+        },
+        Tel: "345312365",
+        role: "wellcVol"
+    },
+
+    {
+        id: "103145",
+        userid: "fazzurretta",
+        name: "Fabio",
+        surname: "Azzurretta",
+        address: {
+            street: "viale Genova, 28",
+            town: "Cesena",
+            province: "Forlì-Cesena",
+            CAP: "47128"
+        },
+        Tel: "345345234",
+        role: "wellcVol"
+    }
+]
+
+module.exports = {persone}
