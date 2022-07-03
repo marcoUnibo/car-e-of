b95ceae3bb506426a6290c1f7f533f955b4f0af1
Marco <50715236+marcofurno@users.noreply.github.com>
2022-07-03 22:03:17 +0200

    Completed CRUD

--- routes/persons.js
+++ routes/persons.js
@@ -1,28 +1,31 @@
-const express = require("express")
+const express = require("express") //const express = require("express")
 const router = express.Router()
-const {persone} = require("../persons")
+//import {listPersons} from "../controllers/persons.js"
 
+let { persone } = require("../persons") //let and not const otherwise it is not possible to delete
 
-router.get("/", (req, res) =>{
-	const newPersons = persone.map((persona) =>{
-		const {id, name, surname, role} = persona
-		return {id, name, surname, role}
+router.get("/",  (req, res) => {
+	const newPersons = persone.map((persona) => {
+		const { id, name, surname, role } = persona
+		return { id, name, surname, role }
 	})
 	// res.send("This is the persons page. You can go to <a href='/'>Home</a>") REMEMBER: Each request may have only one response
 	// res.send("This is the second persons page. You can go to <a href='/'>Home</a>") 
 	res.status(200).json(newPersons) // to see all the information: res.json(persone)
-	
+
 })
+
+//router.get("/", listPersons)
 
 
 router.get("/:idPerson", (req, res) => {
 	console.log(req.params)
-	const {idPerson} = req.params
-	const singlePerson = persone.find((singlePerson) =>	singlePerson.id === idPerson) //if the id is a number, write: singlePerson.id === Number(id)
+	const { idPerson } = req.params
+	const singlePerson = persone.find((singlePerson) => singlePerson.id === idPerson) //if the id is a number, write: singlePerson.id === Number(id)
 
-	if (!singlePerson){
+	if (!singlePerson) {
 		//return res.status(404).send("Person not found") REMEMBER: Each request may have only one response
-		return res.status(404).send({message: "person not found", error: "404"})
+		return res.status(404).send({ message: "person not found", error: "404" })
 	}
 	res.json(singlePerson)
 })
@@ -36,25 +39,55 @@ router.get("/:idPerson", (req, res) => {
 // Attention this post do not check anything about the new person pushed
 router.post("/", (req, res) => {
 	const singlePerson = req.body
+	for (let i in persone) {
+		if (req.body.id == persone[i].id) {
+			res.status(404).json({ success: false, data: persone })
+			return
+		}
+	}
 	persone.push(singlePerson)
-	res.status(200).json({success: true, data: persone})
+	res.status(200).json({ success: true, data: persone })
 })
 
 router.put("/:idPerson", (req, res) => {
-	const {idPerson} = req.params
+	const { idPerson } = req.params
 	const singlePerson = req.body
 	const index = persone.findIndex(singlePerson => singlePerson.id === idPerson)
 	persone[index] = singlePerson //for changing all the data
-//	persone[index].name = singlePerson.name // for changing only the name
-	res.status(200).json({success: true, data: persone})
+	//	persone[index].name = singlePerson.name // for changing only the name
+	res.status(200).json({ success: true, data: persone })
 
+})
+
+
+router.patch("/:idPerson", (req, res) => {
+	const { idPerson } = req.params
+	//const { name, surname} = req.body
+	const {userid, name, surname, address: {street, town, province, CAP}, Tel, role} = req.body
+	let personFind = persone.find((person) => person.id == idPerson)
+	
+
+	if (name) personFind.name = name;
+	if (surname) personFind.surname = surname;
+	if (street) personFind.address.street = street
+	if (Tel) personFind.address.Tel = Tel
+
+	//persone = persone.filter((person) => person.id != idPerson)
+	res.status(200).json({ success: true, data: persone })
 })
 
 router.delete("/:idPerson", (req, res) => {
-	const {idPerson} = req.params
-	const index = persone.findIndex(singlePerson => singlePerson.id === idPerson)
-	persone.splice(index,1)
-	res.status(200).json({success: true, data: persone})
+	const { idPerson } = req.params
+	for (let i in persone) {
+		if (req.body.idPerson != persone[i].id) {
+			res.status(404).json({ success: false, data: persone })
+			return
+		}
+	}
+	persone = persone.filter((person) => person.id != idPerson)
+	res.status(200).json({ success: true, data: persone })
 })
+
+
 
 module.exports = router
