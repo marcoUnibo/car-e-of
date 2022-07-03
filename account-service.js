3853ef63300687456d6dd8b16178447744818660
Marco <50715236+marcofurno@users.noreply.github.com>
2022-07-03 15:33:06 +0200

    added new file

---
+++ account-service.js
@@ -0,0 +1,164 @@
+/* ACCOUNT MICRO-SERVICE */
+  
+const express  = require("express");
+const app = express()
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.json()); 
+
+const axios = require("axios");
+
+//const { resetWatchers } = require("nodemon/lib/monitor/watch");
+
+const {persone} = require("./persons")
+app.use(express.static("public"))
+
+const middleware = require("./middleware")
+app.use("/persons",middleware) //in this way, we limit the use of middleware to only the end points (routes) with /persons
+//app.use([middleware, middleware2, middleware3, auth,]) it is possible use more middleware for end point, creating an array of them
+// app.use(express.json()) This middleware let us to read all the input json 
+
+/* --------------------------------------------------------------------- */
+
+const PORT  = 5050
+
+var model = require('./account-model'); //TO DEL
+
+
+// app.get("/", middleware, (req, res) => {...}  it is a way to put single middleware for a specific end point
+
+app.get("/", (req, res) => {
+	res.send("This is the account service. You can go to <a href='/persons'>persone</a>")
+})
+
+
+// Create a static public page HTML (see express send html page)
+app.get("/homepage", async (req, res) =>{
+	res.sendFile("homepage.html", {root: __dirname + "/public"})
+})
+
+
+app.get("/persons", (req, res) =>{
+	const newPersons = persone.map((persona) =>{
+		const {id, name, surname, role} = persona
+		return {id, name, surname, role}
+	})
+	// res.send("This is the persons page. You can go to <a href='/'>Home</a>") REMEMBER: Each request may have only one response
+	// res.send("This is the second persons page. You can go to <a href='/'>Home</a>") 
+	res.status(200).json(newPersons) // to see all the information: res.json(persone)
+	
+})
+
+
+app.get("/persons/:idPerson", (req, res) => {
+	console.log(req.params)
+	const {idPerson} = req.params
+	const singlePerson = persone.find((singlePerson) =>	singlePerson.id === idPerson) //if the id is a number, write: singlePerson.id === Number(id)
+
+	if (!singlePerson){
+		//return res.status(404).send("Person not found") REMEMBER: Each request may have only one response
+		return res.status(404).send({message: "person not found", error: "404"})
+	}
+	res.json(singlePerson)
+})
+
+//The follow could be a situation of a request for a person that have some other information and I want only a detail
+//like a single transport of that person and not all the transports. It could be something like that:
+// app.get("/person/:idPerson/transports/:idTransport", (req, res)=>{...})
+
+app.get("/searchPersonSurname", (req, res) => {
+	const { query, limit} = req.query
+	let personsFiltered = [...persone]
+	if (query) {
+		personsFiltered = personsFiltered.filter((person) => {
+			return person.surname.startsWith(query)
+		})
+	}
+	if (limit){
+		personsFiltered = personsFiltered.slice(0, Number(limit))
+	}
+	if (personsFiltered.length < 1) {
+		//console.log("yes")
+		return res.status(200).json({Search: true, data: "empty"})
+	}
+	res.status(200).json(personsFiltered)
+	// example how to write the query on the browser: http://127.0.0.1:5050/searchperson?query=M&limit=1
+})
+
+app.get("/searchPersonRole", (req, res) => {
+	const { query, limit} = req.query
+	let personsFiltered = [...persone]
+	if (query) {
+		personsFiltered = personsFiltered.filter((person) => {
+			return person.role.startsWith(query)
+		})
+	}
+	if (limit){
+		personsFiltered = personsFiltered.slice(0, Number(limit))
+	}
+	if (personsFiltered.length < 1) {
+		//console.log("yes")
+		return res.status(200).json({Search: true, data: "empty"})
+	}
+	res.status(200).json(personsFiltered)
+	// example how to write the query on the browser: http://127.0.0.1:5050/searchperson?query=M&limit=1
+})
+
+app.get("/searchPersonId", (req, res) => {
+	const { query, limit} = req.query
+	let personsFiltered = [...persone]
+	if (query) {
+		personsFiltered = personsFiltered.filter((person) => {
+			return person.id.startsWith(query)
+		})
+	}
+	if (limit){
+		personsFiltered = personsFiltered.slice(0, Number(limit))
+	}
+	if (personsFiltered.length < 1) {
+		//console.log("yes")
+		return res.status(200).json({Search: true, data: "empty"})
+	}
+	res.status(200).json(personsFiltered)
+	// example how to write the query on the browser: http://127.0.0.1:5050/searchperson?query=M&limit=1
+})
+
+// Attention this post do not check anything about the new person pushed
+app.post("/persons", (req, res) => {
+	const singlePerson = req.body
+	persone.push(singlePerson)
+	res.status(200).json({success: true, data: persone})
+})
+
+app.put("/persons/:idPerson", (req, res) => {
+	const {idPerson} = req.params
+	const singlePerson = req.body
+	const index = persone.findIndex(singlePerson => singlePerson.id === idPerson)
+	persone[index] = singlePerson //for changing all the data
+//	persone[index].name = singlePerson.name // for changing only the name
+	res.status(200).json({success: true, data: persone})
+
+})
+
+app.delete("/persons/:idPerson", (req, res) => {
+	const {idPerson} = req.params
+	const index = persone.findIndex(singlePerson => singlePerson.id === idPerson)
+	persone.splice(index,1)
+	res.status(200).json({success: true, data: persone})
+})
+
+// This is the last app if your request is wrong. Two different situation
+// first simple response: res.send("<h1>Page not found</h1>")
+// second with res.SendFile to a static page
+
+
+app.all("*", (req, res) => {
+	res.sendFile("404_error.html", {root: __dirname + "/public"}) 
+})
+
+//-----------------------------------------------------------------------------
+
+app.listen(PORT, () => {
+	console.log("Account service up and running - listening on port " + PORT)
+})
